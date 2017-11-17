c13ee8fe11bda3c58d9c196b1d30de1b2719ecd2
jiran1221 <jiran1221@163.com>
2017-11-17 18:28:57 +0800

    init lemview

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <el-container>
+    	<el-header>顶部</el-header>
+    	<el-aside>侧边</el-aside>
+    	<el-main>内容区</el-main>
+    	<el-footer>底部</el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
