ec77288f2431e8c1b8e6bc2fa947ddb88c363ad4
BOBO <1192012793@qq.com>
2017-11-21 15:50:59 +0800

    公共的警告框 需要传alerts数组

---
+++ src/components/common/alert/alert.vue
@@ -0,0 +1,13 @@
+<template>
+    <ul>
+    <li v-for="alert in alerts" :key="alert.id">
+        <el-alert :title="alert.title" :type="alert.type" show-icon>
+        </el-alert>
+    </li>
+    </ul>
+</template>
+<script>
+     export default {
+         props: ['alerts'],
+     }
+</script>
