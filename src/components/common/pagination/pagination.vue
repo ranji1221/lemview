95f3e05f7525bf2ad5e578dd43271a1eb5044e90
BOBO <1192012793@qq.com>
2017-11-21 16:12:48 +0800

    公共分页插件，没改样式

---
+++ src/components/common/pagination/pagination.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="block">
+    <el-pagination
+      :current-page.sync="currentPage"
+      :page-size="page.size"
+      layout="prev, pager, next, jumper"
+      :total="page.total">
+    </el-pagination>
+  </div>
+</template>
+<script>
+  export default {
+    props: ['page'],
+    methods: {
+     
+    },
+    data() {
+      return {
+        currentPage: 1,
+      };
+    }
+  }
+</script>
