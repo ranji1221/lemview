bd919b4caa49e258b30c70f3619156846f810ebc
BOBO <1192012793@qq.com>
2017-11-22 10:28:10 +0800

    用户和资源的列表样式就交给郑龙啦

---
+++ src/components/authority/resource/list.vue
@@ -0,0 +1,121 @@
+<template>
+    <div>
+      <lemon-alert :alerts="alerts"></lemon-alert>
+      <lemon-list class="rolelist" :tabledata="tabledatas"  :items="items"> 
+      </lemon-list>
+      <lemon-pagination :page="page"></lemon-pagination>
+    </div>
+</template>
+
+<script>
+import LemonList from '@/components/common/list/list.vue';
+import LemonAlert from '@/components/common/alert/alert.vue';
+import LemonPagination from '@/components/common/pagination/pagination.vue'
+import "@/assets/style/common/list.css"
+
+export default {
+  components: {
+    LemonList,LemonAlert,LemonPagination
+  },
+  data() {
+    return {
+      page: {
+        size: 10,
+        total: 19
+      },
+      alerts: [{
+        title: '温馨提示：此页面展示角色列表，您可以对角色查看、编辑、删除、授权及批量删除和新建的操作。单击右方x号，可以关闭此条提示语!',
+        type: 'info'
+      }],
+      tabledatas:[
+        {
+          id:'01',
+          resourceType:"菜单",
+          resourceName:"员工",
+          resourcePName:"权限管理"
+        },
+        {
+          id:'02',
+          resourceType:"菜单",
+          resourceName:"员工",
+          resourcePName:"权限管理"
+        },
+       {
+          id:'03',
+          resourceType:"菜单",
+          resourceName:"员工",
+          resourcePName:"权限管理"
+        },
+       {
+          id:'04',
+          resourceType:"菜单",
+          resourceName:"员工",
+          resourcePName:"权限管理"
+        },
+      ],
+      items: [
+      {
+        id: 1,
+        prop:'id',
+        type: " ",
+        label: "编号",
+        sort:true,
+        icon:false
+      },
+      {
+        id: 2,
+        prop:'resourceType',
+        type: " ",
+        label: "资源类型",
+        sort:true,
+        icon:false
+      },
+      {
+        id:3,
+        prop:'resourceName',
+        type:"",
+        label:"资源名称",
+        sort:true,
+        icon:false
+      },
+       {
+        id:4,
+        prop:'resourcePName',
+        type:"",
+        label:"父级资源",
+        sort:true,
+        icon:false
+      },
+      {
+        id:5,
+        type:"",
+        label:"查看",
+        sort:false,
+        icon:'icon-eye-open',
+      },
+      {
+        id:6,
+        type:"",
+        label:"编辑",
+        sort:false,
+        icon:'icon-pencil'
+      },
+      {
+        id:7,
+        type:"",
+        label:"删除",
+        sort:false,
+        icon:'icon-trash',
+      },
+      {
+        id:8,
+        type:"",
+        label:"授权",
+        sort:false,
+        icon:'icon-key'
+      },
+      ],
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,11 @@ import Authority from "@/components/authority/authority.vue"
 import RoleList from "@/components/authority/role/list.vue"
 import RoleAdd from '@/components/authority/role/add.vue'
 import RoleAdds from '@/components/authority/role/adds.vue'
+import UserList from '@/components/authority/user/list.vue'
+import UserAdd from '@/components/authority/user/add.vue'
+import UserAdds from '@/components/authority/user/adds.vue'
+import ResourceList from '@/components/authority/resource/list.vue'
+import ResourceAdd from '@/components/authority/resource/add.vue'
 
 Vue.use(Router)
 
@@ -33,6 +38,31 @@ export default new Router({
           path:"roleAdds",
           name:"角色管理/批量添加角色",
           component:RoleAdds
+        },
+        {
+          path:"userList",
+          name:"用户管理/用户列表",
+          component:UserList
+        },
+        {
+          path:"userAdd",
+          name:"用户管理/添加用户",
+          component:UserAdd
+        },
+        {
+          path:"userAdds",
+          name:"用户管理/批量添加用户",
+          component:UserAdds
+        },
+        {
+          path:"resourceList",
+          name:"资源管理/资源列表",
+          component:ResourceList
+        },
+        {
+          path:"resourceAdd",
+          name:"资源管理/添加资源",
+          component:ResourceAdd
         },
        
       ]
