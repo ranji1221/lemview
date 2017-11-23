cf587449bd4fd88cd79173e2ab38484c63111b6c
lyx <584719733@qq.com>
2017-11-23 22:41:13 +0800

    '用户编辑，资源编辑'

---
+++ src/components/auth/resource/add.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="role_add option_title_agg">
+    <lemon-option-title></lemon-option-title>
+    <lemon-form :forms="form"></lemon-form> 
+  </div>
+</template>
+
+<script>
+import LemonForm  from "@/components/common/action/Form"
+import LemonOptionTitle from "@/components/common/action/OptionTitle"
+import "@/assets/style/common/Option_title.css"
+
+export default {
+    components:{ LemonForm,LemonOptionTitle },
+      data() {
+      return {
+        form: {
+          name: {
+            label:"资源名称",
+            text:"",
+            type:"text",
+            default:"请输入资源名称"
+          },
+          fath:{
+            label:"资源类型",
+            text:"",
+            type:"select",
+            selected:"选择资源类型",
+            childrens:[
+              {
+                label:"资源列表",
+                value:1
+              },
+              {
+                label:"首页",
+                value:2
+              }
+            ]
+          },
+          rely:{
+            label:"父级角色",
+            text:"",
+            type:"select",
+            selected:"依赖父级列表",
+            childrens:[
+              {
+                label:"资源列表",
+                value:1
+              },
+              {
+                label:"首页",
+                value:2
+              }
+            ]
+          },
+          operation:{
+            lable:"相关操作",
+            text:"",
+            type:"checked",
+            childrens:[
+                {
+                    label:"查看",
+                    value:1
+                },
+                {
+                    label:"更改",
+                    vaule:2
+                },
+                {
+                    label:"增加",
+                    value:3
+                },
+                {
+                    label:"删除",
+                    value:4
+                }
+            ]
+          }
+        }
+      }
+    },
+    
+}
+</script>
---
+++ src/components/auth/user/Add.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="user_add option_title_agg">
+    <lemon-option-title></lemon-option-title>
+    <lemon-form :forms="form"></lemon-form> 
+  </div>
+</template>
+
+<script>
+import LemonForm  from "@/components/common/action/Form"
+import LemonOptionTitle from "@/components/common/action/OptionTitle"
+import "@/assets/style/common/Option_title.css"
+
+export default {
+    components:{ LemonForm,LemonOptionTitle },
+      data() {
+      return {
+        form: {
+          name: {
+            label:"用户名称",
+            text:"",
+            type:"text",
+            default:"请输入用户名称"
+          },
+          phone: {
+            label:"联系电话",
+            text:"",
+            type:"text",
+            default:"请输入用户联系方式"
+          },
+          emily: {
+            label:"邮箱",
+            text:"",
+            type:"text",
+            default:"请输入用户邮箱"
+          },
+          fath:{
+            label:"分配用户",
+            text:"",
+            type:"select",
+            selected:"选择分配用户",
+            childrens:[
+              {
+                label:"用户列表",
+                value:1
+              },
+              {
+                label:"首页",
+                value:2
+              }
+            ]
+          },
+        }
+      }
+    },
+    
+}
+</script>
