30dfb8df20a0e9acd4e9301d93fafad71b90abe0
lyx <584719733@qq.com>
2017-11-23 10:08:04 +0800

    上传

---
+++ src/components/auth/role/Adds.vue
@@ -0,0 +1,25 @@
+<template>
+  <lemon-form :forms="form"></lemon-form> 
+</template>
+
+<script>
+import { LemonForm } from "@/components/common/action/form"
+
+export default {
+    components:[LemonForm],
+      data() {
+      return {
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        }
+      }
+    },
+}
+</script>
