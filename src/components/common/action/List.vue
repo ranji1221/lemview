d391475b85eebcd1a6105c8fbab4a2819717566a
bloodkey <3028336816@qq.com>
2017-11-27 08:49:01 +0800

    大早上一来就解决冲突。。。

--- src/components/common/action/List.vue
+++ src/components/common/action/List.vue
@@ -1,13 +1,17 @@
 <template>
+	<div>
+		
+
   <el-table ref="multipleTable" stripe tooltip-effect="dark" style="width: 100%" border :data="tabledata" @selection-change="handleSelectionChange" :default-sort = "{prop: 'id', order: 'descending'}">
+   		<!--是否包含多选框-->
    		<template v-if="actions.selection">
         <el-table-column :resizable="resizable" align="center" type="selection" class-name="tableAction">
         </el-table-column>
       </template>
-      
+      <!--循环数据-->
 	    <el-table-column v-for="item in items" :resizable="resizable"	 align="center" :key="item.id" :label="item.label" :sortable="item.sort" :prop="item.prop" :class-name="item.class">
 	    </el-table-column>
-
+      <!--是否包含查看操作-->
       <template v-if="actions.view">
         <el-table-column :resizable="resizable" align="center" label="查看" class-name="tableAction">
           <template slot-scope="scope">
@@ -15,6 +19,7 @@
           </template>
         </el-table-column>
       </template>
+      <!--是否包含编辑操作-->
       <template v-if="actions.edit">
         <el-table-column :resizable="resizable" align="center" label="编辑" class-name="tableAction">
           <template slot-scope="scope">
@@ -22,6 +27,7 @@
           </template>
         </el-table-column>
       </template>
+      <!--是否包含删除操作-->
       <template v-if="actions.dele">
         <el-table-column :resizable="resizable" align="center" label="删除" class-name="tableAction">
           <template slot-scope="scope">
@@ -29,6 +35,7 @@
           </template>
         </el-table-column>
       </template>
+      <!--是否包含授权操作-->
       <template v-if="actions.ault">
         <el-table-column :resizable="resizable" align="center" label="授权" class-name="tableAction">
           <template slot-scope="scope">
@@ -37,12 +44,75 @@
         </el-table-column>
       </template>
   </el-table>
+
+<!-- Table -->
+<el-button type="text" @click="dialogTableVisible = true">打开嵌套表格的 Dialog</el-button>
+
+
+
+<!-- Form -->
+<el-button type="text" @click="dialogFormVisible = true">打开嵌套表单的 Dialog</el-button>
+
+<el-dialog title="收货地址" :visible.sync="dialogFormVisible" :modal="false" :lock-scroll="false" :close-on-click-modal="false">
+  <el-form :model="form">
+    <el-form-item label="活动名称" :label-width="formLabelWidth">
+      <el-input v-model="form.name" auto-complete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="活动区域" :label-width="formLabelWidth">
+      <el-select v-model="form.region" placeholder="请选择活动区域">
+        <el-option label="区域一" value="shanghai"></el-option>
+        <el-option label="区域二" value="beijing"></el-option>
+      </el-select>
+    </el-form-item>
+  </el-form>
+  <div slot="footer" class="dialog-footer">
+    <el-button @click="dialogFormVisible = false">取 消</el-button>
+    <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+  </div>
+</el-dialog>
+
+
+	
+	</div>
 </template>
 <script>
 export default {
   props: ['items', 'tabledata','actions'],
   data() {
     return {
+    	true:true,
+    	false:false,
+    	 gridData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }],
+        dialogTableVisible: false,
+        dialogFormVisible: false,
+        form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        },
+        formLabelWidth: '120px',
+     
       multipleSelection: [],
       resizable:false,
     }
@@ -68,16 +138,25 @@ export default {
     },
     handleView(index, row) {
 	    console.log(index, row);
-	  },
-	   handleEdit(index, row) {
+	    var dialog=`<el-dialog title="收货地址" :visible.sync="dialogTableVisible" :modal="false" :lock-scroll="false" :close-on-click-modal="false" >
+					  <el-table :data="gridData">
+					    <el-table-column property="date" label="日期" width="150"></el-table-column>
+					    <el-table-column property="name" label="姓名" width="200"></el-table-column>
+					    <el-table-column property="address" label="地址"></el-table-column>
+					  </el-table>
+					</el-dialog>`;
+	    $('.listpagewrap').append(dialog)
+	    console.log(('.listpagewrap') )
+	},
+	handleEdit(index, row) {
 	    console.log(index, row);
-	  },
-	  handleDele(index, row) {
+	},
+	handleDele(index, row) {
 	    console.log(index, row);
-	  },
-	  handleAult(index, row) {
+	},
+	handleAult(index, row) {
 	    console.log(index, row);
-	  },
+	},
   }
 }
 
